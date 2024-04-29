558ef67924987871b739101fe945fa85805ec6b7
kuma214 <hukuma214@gmail.com>
2024-04-29 11:55:18 +0900

    24-04-29.1 commit

--- src/day_4_29/B_Arithmetic_10869.java
+++ src/day_4_29/B_Arithmetic_10869.java
@@ -1,4 +1,32 @@
 package day_4_29;
 
+import java.util.Scanner;
+
 public class B_Arithmetic_10869 {
+    /* 문제
+    두 자연수 A와 B가 주어진다.
+    이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+
+    입력 > 두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+    ex) 7 3
+    출력 > 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B,
+     넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+    ex) 10
+        4
+        21
+        2
+        1
+     */
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
 }
---
+++ src/day_4_29/P_forTest_1.java
@@ -0,0 +1,14 @@
+package day_4_29;
+
+import java.util.Scanner;
+
+public class P_forTest_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) System.out.print(str);
+
+    }
+}
